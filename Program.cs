a2ed0a4620a1b09b03781c34547f5140d5139a82
Gustavo Poeta <gustavpoeta@gmail.com>
2025-12-18 11:28:29 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+namespace sistemaEstoque;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Configuration.AddUserSecrets<Program>();
+        }
+
+        // FRAMEWORK
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
